53b459aac211553352c0e53aa0566c4d1ac6e5e2
yustikamasya <yustikamasya3@gmail.com>
2020-12-21 20:45:03 +0700

    SQLite 0114

---
+++ app/src/main/java/com/example/pcs_sqlite_0114/Note.java
@@ -0,0 +1,32 @@
+package com.example.pcs_sqlite_0114;
+
+public class Note {
+    private int id;
+    private String title;
+    private String note;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
